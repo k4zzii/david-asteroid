cdc521261e47783e42e5398848638bfb85dfbf49
halbeisent <thibault.halbeisendev@gmail.com>
2021-05-17 14:19:19 +0200

    I want to die

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 var canvas = document.getElementById('asteroid');
 var image = document.getElementById('asteroidSVG');
+var spaceBackground = new Image();
+spaceBackground.src = "espace.png"
 var ctx = canvas.getContext('2d');
 ctx.canvas.height = window.innerHeight;
 ctx.canvas.width = window.innerWidth;
-var posX = 200;
-var posY = 300;
+var posX = window.innerWidth;
+var posY = window.innerHeight;
 
 function draw() {
   console.log('draw')
   if (canvas.getContext) {
-    ctx.drawImage(image, posX, posY + increase, 80, 80);
+    ctx.drawImage(spaceBackground, 0, 0)
+    ctx.drawImage(image, posX/2, posY/2 + increase, 80, 80);
     console.log("ok")
   
   } else {
